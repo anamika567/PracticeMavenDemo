1a17b1b754e804da00216c4a66a2cfd1a1382411
hp <hp@DESKTOP-9BB5NPN>
2023-01-24 10:36:17 +0530

    new project

---
+++ src/test/java/Demo/DemoFirstTest.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoFirstTest {
+	
+	@Test
+	public void demo1()
+	{
+		System.out.println("----demo1---");
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("----demo2---");
+	}
+
+}
---
+++ src/test/java/Demo/DemoSecondTest.java
@@ -0,0 +1,18 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoSecondTest {
+	@Test
+	public void demo3()
+	{
+		System.out.println("----demo1---");
+	}
+	
+	@Test
+	public void demo4()
+	{
+		System.out.println("----demo2---");
+	}
+
+}
